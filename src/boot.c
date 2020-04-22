bb697c43dc51b49803fa5e5d6bb8412dc125925e
Reto Achermann <achreto@gmail.com>
2020-04-22 14:18:42 +0200

    initial function in C and the bootloadre_args struct
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- src/boot.c
+++ src/boot.c
@@ -7,3 +7,23 @@
  * Copyright 2020 Reto Achermann
  * SPDX-License-Identifier: GPL-3.0
  */
+
+
+#include <types.h>
+#include <config.h>
+#include <bootloader_args.h>
+#include <serial.h>
+
+struct bootloader_args bootloader_args = BOOTLOADER_ARGS_INIT(CONFIG_BSP_MIPD);
+
+
+
+void bootloader_init(struct bootloader_args *data) __attribute__((noreturn));
+void bootloader_init(struct bootloader_args *data)
+{
+    serial_console_early_init(data->serial[CONFIG_SERIAL_CONSOLE_PORT]);
+    serial_debug_early_init(data->serial[CONFIG_SERIAL_DEBUG_PORT]);
+
+    while(1)
+        ;
+}
