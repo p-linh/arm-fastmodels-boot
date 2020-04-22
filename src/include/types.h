0eb378457a5611413cc69fe9c241e6f1ce2a9498
Reto Achermann <achreto@gmail.com>
2020-04-22 11:52:18 +0200

    adding basic, quick typdefs, revise later
    Signed-off-by: Reto Achermann <achreto@gmail.com>

---
+++ src/include/types.h
@@ -0,0 +1,19 @@
+/*
+ * Arm FastModels Hello World Boot
+ *
+ * Copyright 2020 Reto Achermann
+ * SPDX-License-Identifier: GPL-3.0
+ */
+
+#ifndef __TYPES_H_
+#define __TYPES_H_ 1
+
+typedef unsigned long uint64_t;
+typedef unsigned int uint32_t;
+
+typedef unsigned long uintptr_t;
+
+typedef uintptr_t lpaddr_t;
+typedef uintptr_t lvaddr_t;
+
+#endif /* __TYPES_H_ */
