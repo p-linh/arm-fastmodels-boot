5d9ad76b9373a5a959ae27ff8e53c500da1f8950
Reto Achermann <achreto@gmail.com>
2020-07-29 17:29:57 +0200

    update boot-sequence to enable caches
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- src/boot.c
+++ src/boot.c
@@ -15,17 +15,51 @@
 #include <serial.h>
 #include <stdio.h>
 
+#include <logging.h>
+#include <armv8.h>
+#include <processor.h>
+#include <exceptions.h>
+#include <mmu.h>
+#include <cachectrl.h>
+
 struct bootloader_args bootloader_args = BOOTLOADER_ARGS_INIT(CONFIG_BSP_MIPD,
                                                               CONFIG_SERIAL_CONSOLE_BASE);
+
 
 
 void bootloader_init(struct bootloader_args *data) __attribute__((noreturn));
 void bootloader_init(struct bootloader_args *data)
 {
+    /* disable interrupts */
+    armv8_disable_interrupts();
+
+    /* switch to non-secure world */
+    armv8_switch_to_non_secure_world();
+
+    /* initialize the serial console */
     serial_console_early_init(data->serial[CONFIG_SERIAL_CONSOLE_PORT]);
     serial_debug_early_init(data->serial[CONFIG_SERIAL_DEBUG_PORT]);
 
-    printf("hello world!\n");
+    /* print the boot banner */
+    MSG("###############################################################################\n");
+    MSG("FastModels bootloader starting on %s %s core rev. r%u in EL%u\n",
+        armv8_processor_implementer_string(), armv8_processor_model_string(),
+        armv8_processor_revision_number, armv8_get_current_el());
+    MSG("###############################################################################\n");
+
+    /* setup exception vectors */
+    exceptions_setup_vectors();
+
+    /* configure and enable MMU */
+    // mmu_configure_and_enable();
+
+    /* enable caches */
+    cachectrl_enable_all_caches();
+
+
+    MSG("Skipping lower EL configuration.");
+    MSG("Reached end of initialization sequence...\n");
+
 
     while (1)
         ;
