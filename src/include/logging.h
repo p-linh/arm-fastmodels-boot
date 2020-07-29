06573c3645d16847f54b4c85b1c9b5b8d1ef90ee
Reto Achermann <achreto@gmail.com>
2020-07-29 17:28:51 +0200

    adding header guard in logging.h
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- src/include/logging.h
+++ src/include/logging.h
@@ -5,9 +5,16 @@
  * SPDX-License-Identifier: GPL-3.0
  */
 
+#ifndef LOGGING_H_
+#define LOGGING_H_ 1
+
+#include <stdio.h>
+
 ///< a basic message printing macro
 #define MSG(format, ...) printf("[ARMv8]: " format, ## __VA_ARGS__ )
 
 #define WARN(format, ...) printf("[ARMv8]: WARNING " format, ## __VA_ARGS__ )
 
 #define ERROR(format, ...) printf("[ARMv8]: " format, ## __VA_ARGS__ )
+
+#endif /* LOGGING_H_ */
